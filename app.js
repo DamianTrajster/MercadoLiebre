eac41b0760da0ad75335daa64633c6149f1baef4
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-06-02 18:38:49 -0300

    HTML mercado Liebre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+//CREAR LAS RUTAS
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname,'./views','home.html')));
+
+
+
+//ESCUCHAR EL SERVIDOR
+app.listen (port, ()=> console.log(`Servidor corriendo de manera satisfactoria: `+ port ));
